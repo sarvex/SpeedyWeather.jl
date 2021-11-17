6ed0f12954e15af4129901207800fa62d00a9e1f
Documenter.jl <documenter@juliadocs.github.io>
2021-11-17 18:17:47 +0000

    build based on 25ab344

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SpeedyWeather.jl-documentation","page":"Home","title":"SpeedyWeather.jl documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"test","category":"page"},{"location":"","page":"Home","title":"Home","text":"a = 1","category":"page"}]
+}
